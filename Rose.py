d2beb1014fc09339725f2d7f675fc0f4eac163c5
Dale Chou <dale@distract.org>
2017-10-10 19:39:32 +0800

    Update Rose.py

--- Rose.py
+++ Rose.py
@@ -1,14 +1,15 @@
+import base64
 import linecache
 
 # Grab source_name.
 target_order = 0
-source_name = raw_input("Enter a name: ")
+source_name = input("Enter a name: ")
 list_of_names = r"census-derived-all-first.txt"
 
 # Encode source_name.
-for encoded_char in source_name.encode("base64", "strict"):
-    target_order = target_order + ord(encoded_char)
+for encoded_char in base64.b64encode(bytes(source_name, "utf-8")):
+    target_order = target_order + encoded_char
 
 # Pick a name.
 target_order = target_order % len(open(list_of_names).readlines())
-print "Recommended pseudonym: " + linecache.getline(list_of_names, target_order).partition(" ")[0]
+print("Recommended pseudonym: " + linecache.getline(list_of_names, target_order).partition(" ")[0])
